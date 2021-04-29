58afb160c2cba3da3a69043f4fe0409633e486b8
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-04-29 11:46:54 +0200

    Update parser_test.py
    TestParseMusicalNote

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,8 +1,8 @@
 import unittest
-from parser.parser import musical_notes, parse_midi_note
+from parser.parser import musical_notes, parse_midi_note, parse_musical_notes
 
 
-class TestParse(unittest.TestCase):
+class TestParseMidiNote(unittest.TestCase):
     def test_f_minus_1(self):
         self.assertEqual(parse_midi_note(5), 'F')
 
@@ -14,6 +14,20 @@ class TestParse(unittest.TestCase):
 
     def test_f_diesis_9(self):
         self.assertEqual(parse_midi_note(126), 'F#')
+
+
+class TestParseMusicalNote(unittest.TestCase):
+    def test_R_CM(self):
+        self.assertEqual(parse_musical_notes('R', 'CM'), 'r')
+
+    def test_E_CM(self):
+        self.assertEqual(parse_musical_notes('E', 'CM'), 'c')
+
+    def test_B_CM(self):
+        self.assertEqual(parse_musical_notes('B', 'CM'), 'l')
+
+    def test_B_diesis_CM(self):
+        self.assertEqual(parse_musical_notes('B#', 'CM'), 'x')
 
 
 if __name__ == '__main__':
