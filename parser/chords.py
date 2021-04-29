35e4f5c20c4e36669a76ab3613f29cd48e6cffb7
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-04-29 11:27:07 +0200

    Create chords.py

---
+++ parser/chords.py
@@ -0,0 +1,12 @@
+chord_tones = {
+    'CM': {
+        'c': ['C', 'E', 'G'],
+        'l': ['B', 'D',  'F', 'A']
+    },
+
+    'Am': {
+        'c': ['A', 'C', 'E'],
+        'l': ['G', 'B', 'D', 'F']
+    }
+    # TODO: continue the dictionary
+}
