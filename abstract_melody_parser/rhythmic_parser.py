d517dce4a09d90d383662a57eedd87975e475fb7
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-05-05 10:53:00 +0200

    Update rhythmic_parser.py
    get_nearest_rhythm function completed

--- abstract_melody_parser/rhythmic_parser.py
+++ abstract_melody_parser/rhythmic_parser.py
@@ -111,17 +111,6 @@ class MidiNoteQueue:
             del self._container[index]  # removing the open note_on message
             self._open_note_on_list.remove(open_note)  # removing the open note reference
 
-# {
-#         '1': beat_time * 4,
-#         '2': beat_time * 2,
-#         '4': beat_time * 1,
-#         '4dot': beat_time * 3 / 2,
-#         '4t': beat_time * 2 / 3,
-#         '8': beat_time * 1 / 2,
-#         '8t': beat_time * 1 / 3,
-#         '16': beat_time * 1 / 4,
-#         '16t': beat_time * 1 / 6,
-#     }
 
 def get_nearest_rhythm(interval, rhythmical_durations):
     """
@@ -132,10 +121,27 @@ def get_nearest_rhythm(interval, rhythmical_durations):
     :param rhythmical_durations: dictionary returned by the get_durations
     :return:
     """
+
+    # the keys and the values obtained from the methods "values" and "keys"
+    # of a dictionary are correctly ordered, so we can calculate
+    # the distances from the keys, get the index of the argmin
+    # and use it to get the correct key
+
+    # values of the rhythmical_durations dictionary
     rhythmical_durations_values = list(rhythmical_durations.values())
+
+    # keys of the rhythmical_durations dictionary
+    rhythmical_durations_keys = list(rhythmical_durations.keys())
+
+    # list comprehension used to map the distance function to the values
     distances = [abs(interval - x) for x in rhythmical_durations_values]
+
+    # argmin of the distance (an index)
     result_index = distances.index(min(distances))
-    
+
+    # using the index to get the correct rhythmical duration key
+    return rhythmical_durations_keys[result_index]
+
 
 def parse_rhythm(midi_queue, rhythmical_durations):
     result = []  # this list will contain the rhythmical symbols
@@ -147,7 +153,8 @@ def parse_rhythm(midi_queue, rhythmical_durations):
                 j = j + 1
             # from here on j is the index of the note_off
             interval = midi_queue[j]['timestamp'] - midi_queue[i]['timestamp']
-            # TODO: create distance function
+
+            # getting the closest rhythmic figure and adding it to the result
             rhythm = get_nearest_rhythm(interval, rhythmical_durations)
             result.append(rhythm)
     return result
