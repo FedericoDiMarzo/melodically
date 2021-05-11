dbf6a17641d1bb7889e279772df422723e26e4d8
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-05-11 17:06:49 +0200

    Update rhythm.py

--- abstract_melody_parser/rhythm.py
+++ abstract_melody_parser/rhythm.py
@@ -51,27 +51,4 @@ def get_nearest_rhythm(interval, rhythmical_durations):
     return rhythmical_durations_keys[result_index]
 
 
-def parse_rhythm(midi_queue, rhythmical_durations):
-    """
-    Parse a midi
-    :param midi_queue:
-    :param rhythmical_durations:
-    :return:
-    """
-    # TODO: parse the rests too?
-    result = []  # this list will contain the rhythmical symbols
-    midi_queue_container = midi_queue.get_container()
-    for i in range(len(midi_queue_container)):
-        if midi_queue_container[i]['type'] == 'note_on':
-            j = i + 1
-            # finding the corresponding subsequent note_off
-            while not (midi_queue_container[j]['type'] == 'note_off' and midi_queue_container[j]['note'] ==
-                       midi_queue_container[i]['note']):
-                j = j + 1
-            # from here on j is the index of the note_off
-            interval = midi_queue_container[j]['timestamp'] - midi_queue_container[i]['timestamp']
 
-            # getting the closest rhythmic figure and adding it to the result
-            rhythm = get_nearest_rhythm(interval, rhythmical_durations)
-            result.append(rhythm)
-    return result
