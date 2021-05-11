792d9fa0abb59ddfb090763a52c7a705c4eaee49
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-05-11 19:00:29 +0200

    Update harmonic_state.py
    get_mode_notes and comments

--- abstract_melody_parser/harmonic_state.py
+++ abstract_melody_parser/harmonic_state.py
@@ -3,20 +3,46 @@ from abstract_melody_parser.harmony import midi_to_std, get_root, harmonic_affin
 
 # TODO: comments
 class HarmonicState:
+    """
+    This class allows to monitor and update an internal harmonic state,
+    influenced by external notes inputs
+    """
+
     def __init__(self, buffer_size=16):
+
+        # contains the input notes in std notation
         self.noteBuffer = []
-        self.bufferSize = buffer_size  # TODO: test this value
+
+        # max size of the buffer
+        self.bufferSize = buffer_size
+
+        # mode dictionary used to represent a modal scale
+        # root: root note of the scale
+        # mode_signature_index: index of the interval scheme of a modal scale
+        # mode_index: index of the mode inside a certain interval scheme
         self.currentMode = {'root': 'C', 'mode_signature_index': 0, 'mode_index': 0}
 
     def push_notes(self, new_notes):
+        """
+        Pushes new note inside the buffer.
+        If the buffer overflows, the older notes are discarded.
+
+        :param new_notes: list of new notes
+        """
         self.noteBuffer = self.noteBuffer + new_notes
         while len(self.noteBuffer) > self.bufferSize:
             self.noteBuffer.pop(0)  # removing old notes
 
     def change_mode(self):
+        """
+        Updates the currentMode attribute based on the notes in the buffer,
+        applying the harmonic_affinities function to them.
+
+        :return: currentMode
+        """
         notes_std = [n for n in self.noteBuffer]
         root = get_root(notes_std)
-        modes_affinities = harmonic_affinities(modes_dict[root], notes_std)
+        modes_affinities = harmonic_affinities(root, notes_std)
         mode_signature_index = modes_affinities.index(max(modes_affinities))
         tmp = modes_affinities[mode_signature_index]  # sequence with the lowest distance
         mode_index = tmp.index(max(tmp))
@@ -26,3 +52,14 @@ class HarmonicState:
         self.currentMode['mode_index'] = mode_index
 
         return self.currentMode
+
+    def get_mode_notes(self):
+        """
+        Gets the current notes of the current modal scale.
+
+        :return: list of notes of the current modal scale
+        """
+        self.change_mode()
+        tmp = modes_dict[self.currentMode['root']]
+        tmp = tmp[self.currentMode['mode_signature_index']]
+        return tmp[self.currentMode['mode_index']]
