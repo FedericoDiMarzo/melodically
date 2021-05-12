6a350c7413aa5ec523d7f3eb03dcac9ca18a97bc
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-05-12 13:24:48 +0200

    Update __init__.py
    imports

--- melodically/__init__.py
+++ melodically/__init__.py
@@ -1,5 +1,6 @@
 from melodically.midi_note_queue import *
 from melodically.harmonic_state import *
 from melodically.harmony import *
+from melodically.chords import chord_tones, chord_to_midi
 from melodically.rhythm import *
 from melodically.parsers import *
